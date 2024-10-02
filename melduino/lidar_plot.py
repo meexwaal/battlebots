8de87fdd7803a573e3f9f7ad332bcdc81cfcd4ca
Maxwell Johnson <maximusjohnsonus@gmail.com>
2024-10-01 22:55:12 -0700

    Show rotation rate

--- melduino/lidar_plot.py
+++ melduino/lidar_plot.py
@@ -12,6 +12,7 @@ with open('protocol.h', 'r') as protocol_file:
     ffi.cdef(protocol_file.read())
 
 ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# ServerSocket.settimeout(1)
 host = '0.0.0.0'
 port = 2390
 
@@ -23,6 +24,7 @@ except socket.error as e:
 print("bound")
 
 prev_theta = 0
+prev_time = time.time()
 
 plot_lidar_xy = np.zeros((100, 2))
 
@@ -39,21 +41,29 @@ def animate(save=False):
     # Draw lidar history
     plot_lidar, = ax.plot([], [], 'ro', markersize=2)
 
+    # Show some text
+    plot_text = ax.text(-6, 6, 'foo')
+
     def init():
-        ax.set_xlim(-6, 6)
-        ax.set_ylim(-6, 6)
+        ax.set_xlim(-7, 7)
+        ax.set_ylim(-7, 7)
 
         ax.add_patch(bot_circle)
-        return plot_lidar, bot_circle, orientation
+        return plot_lidar, bot_circle, orientation, plot_text
 
     def update(_):
         global prev_theta
+        global prev_time
 
-        telem_packet, address = ServerSocket.recvfrom(4096)
-        telem_struct = ffi.from_buffer("telem_packet_t *", telem_packet)
+        try:
+            telem_packet, address = ServerSocket.recvfrom(4096)
+            telem_struct = ffi.from_buffer("telem_packet_t *", telem_packet)
 
-        theta = telem_struct.theta
-        rs = np.array(list(telem_struct.lidar_mm)) / 1e3
+            theta = telem_struct.theta
+            rs = np.array(list(telem_struct.lidar_mm)) / 1e3
+        except socket.timeout:
+            theta = np.random.rand()
+            rs = [np.random.rand()] * 16
 
         # while theta + np.pi < prev_theta:
         #     theta += 2 * np.pi
@@ -65,19 +75,28 @@ def animate(save=False):
         #     prev_theta -= 2 * np.pi
 
         # thetas = np.linspace(theta, prev_theta, num=16, endpoint=False)[::-1]
-        # prev_theta = theta
+        dtheta = np.diff(np.unwrap([prev_theta, theta]))[0]
+        now = time.time()
+        dt = now - prev_time
+        prev_theta = theta
+        prev_time = now
 
         # Plot measured theta
         orientation.set_data(
             [1.1 * bot_radius * np.cos(theta)],
             [1.1 * bot_radius * np.sin(theta)])
 
+        # Show rotation speed
+        rps = dtheta / dt / (2 * np.pi)
+        rpm = rps * 60
+        plot_text.set_text(f"{rps:.2f} rps, {rpm:.0f} rpm")
+
         # plot_lidar.set_data(rs * np.cos(thetas), rs * np.sin(thetas))
         plot_lidar_xy[:-1,:] = plot_lidar_xy[1:,:]
         plot_lidar_xy[-1] = (rs[0] * np.cos(theta), rs[0] * np.sin(theta))
         plot_lidar.set_data(plot_lidar_xy[:,0], plot_lidar_xy[:,1])
 
-        return plot_lidar, bot_circle, orientation
+        return plot_lidar, bot_circle, orientation, plot_text
 
 
     ani = FuncAnimation(
